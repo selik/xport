40608ae1a5d6c675b5f7098e22801a7747569a92
Mike Selik <mselik@suplari.com>
2020-04-17 21:02:39 -0700

    Provide Pandas DataFrame and Series accessors.
    Rather than using composition to create ``Member`` classes that contain
    Pandas dataframes, it seems cleaner to provide a ``sas`` accessor for
    Pandas dataframes and series. Each dataset now behaves exactly like a
    regular dataframe, but has the capacity for adding SAS-style metadata.

--- src/xport/__init__.py
+++ src/xport/__init__.py
@@ -3,13 +3,15 @@ Read and write SAS XPORT/XPT-format files.
 """
 
 # Standard Library
-import itertools
+import enum
 import logging
-import pathlib
+import re
+import warnings
+from collections.abc import MutableMapping
+from datetime import datetime
 
-# Xport Modules
-import xport.v56
-from xport.v56 import Library, Member, dump, dumps
+# Community Packages
+import pandas as pd
 
 from .__about__ import __version__  # noqa: F401
 
@@ -18,45 +20,458 @@ LOG = logging.getLogger(__name__)
 __all__ = [
     'Library',
     'Member',
-    'load',
-    'loads',
-    'dump',
-    'dumps',
 ]
 
-formats = {
-    '.xpt': [xport.v56],
-}
 
-
-def load(fp):
+class VariableType(enum.IntEnum):
     """
-    Deserialize a SAS file format document::
-
-        with open('example.xpt', 'rb') as f:
-            data = load(f)
+    SAS variables can be either Numeric or Character type.
     """
-    modules = formats[pathlib.Path(fp.name).suffix]
-    for fmt in modules:
+    NUMERIC = 1
+    CHARACTER = 2
+
+
+class FormatAlignment(enum.IntEnum):
+    """
+    SAS formats are either left- or right-aligned.
+    """
+    LEFT = 0
+    RIGHT = 1
+
+
+class Informat:
+    """
+    SAS variable informat.
+    """
+
+    patterns = {
+        'Character': re.compile(r'^\$(?P<name>[A-Z0-9]*?)(?P<w>\d+)\.$', re.IGNORECASE),
+        'Numeric': re.compile(r'^(?P<name>[A-Z0-9]*?)(?P<w>\d+)\.(?P<d>)?$', re.IGNORECASE),
+        'Date/Time': re.compile(r'^(?P<name>[A-Z0-9]*?)(?P<w>\d+)\.$', re.IGNORECASE),
+    }
+
+    def __init__(self, spec):
+        """
+        Create an informat from a text specification.
+        """
+        for vtype, pattern in self.patterns.items():
+            mo = pattern.fullmatch(spec)
+            if mo is not None:
+                break
+        else:
+            raise ValueError(f'Unrecognized informat {spec}')
+        self._name = mo['name'].upper()
+        if len(self._name) > 8:
+            raise ValueError(f'Format name {self._name} is longer than 8 characters')
+        self._length = int(mo.group('w'))
         try:
-            return fmt.load(fp)
-        except Exception:
-            LOG.exception('Could not parse file with %s', fmt.__name__)
-    raise ValueError('Could not parse file')
+            self._decimals = int(mo.group('d'))
+        except IndexError:
+            self._decimals = None
+
+    @property
+    def name(self):
+        """
+        The name of the format.
+        """
+        return self._name
+
+    @property
+    def length(self):
+        """
+        The width value of the format: ``INFORMATw.``.
+        """
+        return self._length
+
+    @property
+    def decimals(self):
+        """
+        The ``d`` value of numeric formats: ``INFORMATw.d``.
+        """
+        # The documentation states that ``d`` optionally specifies the
+        # power of 10 by which to divide numeric input.  If the data
+        # contain decimal points, the ``d``` value is ignored.
+        return self._decimals
 
 
-def loads(s):
+class Format(Informat):
     """
-    Deserialize a SAS file format document from a string::
-
-        with open('example.xpt', 'rb') as f:
-            bytestring = f.read()
-        data = loads(bytestring)
+    SAS variable format.
     """
-    modules = itertools.chain.from_iterable(formats.values())
-    for fmt in modules:
-        try:
-            return fmt.loads(s)
-        except Exception:
-            LOG.exception('Could not parse file with %s', fmt.__name__)
-    raise ValueError('Could not parse file')
+
+    def __init__(self, name, justify=FormatAlignment.LEFT):
+        """
+        Create a format from a text specification.
+        """
+        super().__init__(name)
+        self.justify = justify
+
+
+@pd.api.extensions.register_series_accessor('sas')
+class SASSeriesAccessor:
+    """
+    Access SAS metadata from a Pandas Series.
+    """
+
+    def __init__(self, series):
+        """
+        Initialize the SAS metadata accessor.
+        """
+        self._series = series
+        if series.name is not None:
+            self.name = self.label = series.name
+        else:
+            self.name = self.label = ''
+        self.format = None
+        self.iformat = None
+
+    @property
+    def name(self):
+        """
+        SAS variable name.
+        """
+        return self._name.decode('ascii')
+
+    @name.setter
+    def name(self, value):
+        value = value.encode('ascii')
+        if len(value) > 32:
+            raise ValueError(f'SAS variable name {value} is longer than 32 characters.')
+        if len(value) > 8:
+            warnings.warn(
+                f'SAS variable name {value} is longer than 8 characters.  SAS'
+                'Version 5 or 6 Transport (XPORT) Format limits names to 8 characters.'
+            )
+        self._name = value
+
+    @property
+    def label(self):
+        """
+        SAS variable label.
+        """
+        return self._label.decode('ascii')
+
+    @label.setter
+    def label(self, value):
+        value = value.encode('ascii')
+        if len(value) > 256:
+            raise ValueError(f'SAS variable label {value} is longer than 256 characters.')
+        if len(value) > 40:
+            warnings.warn(
+                f'SAS variable label {value} is longer than 40 characters.  SAS'
+                'Version 5 or 6 Transport (XPORT) Format limits labels to 40 characters.'
+            )
+        self._label = value
+
+    @property
+    def type(self):
+        """
+        SAS variable type, either numeric or character.
+        """
+        dtype = self._series.dtype
+        if dtype.kind in {'f', 'i'}:
+            return VariableType.NUMERIC
+        elif dtype.kind == 'O':
+            return VariableType.CHARACTER
+        raise TypeError(f'{type(self._series).__name__}.dtype {dtype} not supported')
+
+    @property
+    def length(self):
+        """
+        SAS variable maximum length in bytes.
+        """
+        if self.type == VariableType.NUMERIC:
+            return 8
+        return self._series.str.len().max()
+
+    @property
+    def number(self):
+        """
+        Index of the SAS variable within the dataset.
+        """
+        raise NotImplementedError('Check ``DataFrame.sas.contents`` instead')
+
+    @property
+    def position(self):
+        """
+        Byte-index of the SAS variable field within an observation.
+        """
+        raise NotImplementedError('Check ``DataFrame.sas.contents`` instead')
+
+    @property
+    def format(self):
+        """
+        SAS variable format.
+        """
+        return self._format
+
+    @format.setter
+    def format(self, value):
+        if value is None:
+            self._format = None
+        else:
+            self._format = Format(value)
+
+    @property
+    def iformat(self):
+        """
+        SAS variable informat.
+        """
+        return self._iformat
+
+    @iformat.setter
+    def iformat(self, value):
+        if value is None:
+            self._format = None
+        else:
+            self._iformat = Informat(value)
+
+
+@pd.api.extensions.register_dataframe_accessor('sas')
+class SASDataFrameAccessor:
+    """
+    Access SAS metadata from a Pandas DataFrame.
+    """
+
+    def __init__(self, dataframe):
+        """
+        Initialize the SAS metadata accessor.
+        """
+        self._df = dataframe
+        self.name = self.label = ''
+        self.type = 'DATA'
+        self.modified = self.created = datetime.now()
+        self.os = ''
+        self.version = ''
+
+    @property
+    def contents(self):
+        """
+        Variable metadata, such as label, format, number, and position.
+        """
+        # TODO: Can this dataframe be read-only?
+        df = pd.DataFrame({
+            'Variable': [c.sas.name for k, c in self._df.items()],
+            'Type': [c.sas.type for k, c in self._df.items()],
+            'Length': [c.sas.length for k, c in self._df.items()],
+            'Format': [str(c.sas.format) for k, c in self._df.items()],
+            'Informat': [str(c.sas.informat) for k, c in self._df.items()],
+            'Label': [c.sas.label for k, c in self._df.items()],
+        })
+        df.index.name = '#'
+        df['Position'] = df['Length'].cumsum()
+        return df
+
+    @property
+    def name(self):
+        """
+        Dataset name.
+        """
+        return self._name.decode('ascii')
+
+    @name.setter
+    def name(self, value):
+        value = value.encode('ascii')
+        if len(value) > 8:
+            raise ValueError(f'Dataset name {value} is longer than 8 characters')
+        self._name = value
+
+    @property
+    def label(self):
+        """
+        Dataset label.
+        """
+        return self._label.decode('ascii')
+
+    @label.setter
+    def label(self, value):
+        value = value.encode('ascii')
+        if len(value) > 40:
+            raise ValueError(f'Dataset label {value} is longer than 40 characters')
+        self._label = value
+
+    @property
+    def type(self):
+        """
+        Dataset type.
+        """
+        return self._type.decode('ascii')
+
+    @type.setter
+    def type(self, value):
+        value = value.encode('ascii')
+        if len(value) > 8:
+            raise ValueError(f'Dataset type {value} is longer than 8 characters')
+        self._type = value
+
+    @property
+    def created(self):
+        """
+        Dataset created.
+        """
+        return self._created
+
+    @created.setter
+    def created(self, value):
+        if not isinstance(value, datetime):
+            raise TypeError(f'Expected datetime, not {type(value).__name__}')
+        if not (datetime(1900, 1, 1) < value < datetime(2100, 1, 1)):
+            raise ValueError('Datetime must be in 1900s or 2000s')
+        self._created = value
+
+    @property
+    def modified(self):
+        """
+        Dataset modified.
+        """
+        return self._modified
+
+    @modified.setter
+    def modified(self, value):
+        if not isinstance(value, datetime):
+            raise TypeError(f'Expected datetime, not {type(value).__name__}')
+        if not (datetime(1900, 1, 1) < value < datetime(2100, 1, 1)):
+            raise ValueError('Datetime must be in 1900s or 2000s')
+        self._modified = value
+
+    @property
+    def os(self):
+        """
+        OS used to create the dataset.
+        """
+        return self._os.decode('ascii')
+
+    @os.setter
+    def os(self, value):
+        value = value.encode('ascii')
+        if len(value) > 8:
+            raise ValueError(f'OS name {value} is longer than 8 characters')
+        self._os = value
+
+    @property
+    def version(self):
+        """
+        SAS version used to create the dataset.
+        """
+        return self._version.decode('ascii')
+
+    @version.setter
+    def version(self, value):
+        value = value.encode('ascii')
+        if len(value) > 8:
+            raise ValueError(f'SAS version {value} is longer than 8 characters')
+        self._version = value
+
+
+class Member(pd.DataFrame):
+    """
+    SAS library member.
+    """
+
+
+class Library(MutableMapping):
+    """
+    A collection of datasets from a SAS file.
+    """
+
+    def __init__(self, members, created=None, modified=None, os='', version=''):
+        """
+        Initialize a SAS data library.
+        """
+        self._members = {}
+        for name, dataframe in members.items():
+            if dataframe.sas.name is None:
+                raise ValueError('')
+            self[name] = dataframe
+
+        if created is None:
+            self.created = datetime.now()
+        else:
+            self.created = created
+
+        if modified is None:
+            self.modified = self.created
+        else:
+            self.modified = modified
+
+        self.os = os
+        self.version = version
+
+    @property
+    def version(self):
+        """
+        SAS version used to create the dataset library.
+        """
+        return self._version.decode('ascii')
+
+    @version.setter
+    def version(self, value):
+        value = value.encode('ascii')
+        if len(value) > 8:
+            raise ValueError('SAS version must be <= 8 characters')
+        self._version = value
+
+    @property
+    def os(self):
+        """
+        Operating system used to create the dataset library.
+        """
+        return self._os.decode('ascii')
+
+    @os.setter
+    def os(self, value):
+        value = value.encode('ascii')
+        if len(value) > 8:
+            raise ValueError('SAS os must be <= 8 characters')
+        self._os = value
+
+    def __repr__(self):
+        """
+        REPL-format string.
+        """
+        fmt = '<{cls} members={members}>'
+        return fmt.format(cls=type(self).__name__, members=list(self))
+
+    def __getitem__(self, name):
+        """
+        Get a member dataset.
+        """
+        return self._members[name]
+
+    def __setitem__(self, name, dataframe):
+        """
+        Insert or update a member in the library.
+        """
+        if dataframe.sas.name is None:
+            dataframe.sas.name = name
+            LOG.debug(f'Set dataframe.sas.name to {name!r}')
+        elif name != dataframe.sas.name:
+            msg = 'Library member name {a} must match dataset name {b}'
+            raise ValueError(msg.format(a=name, b=dataframe.sas.name))
+        self._members[name] = dataframe
+
+    def __delitem__(self, name):
+        """
+        Remove a member datset.
+        """
+        del self._members[name]
+
+    def __iter__(self):
+        """
+        Get an iterator of dataset names.
+        """
+        return iter(self._members)
+
+    def __len__(self):
+        """
+        Get the number of datasets in the library.
+        """
+        return len(self._members)
+
+    def __eq__(self, other):
+        """
+        Compare equality.
+        """
+        same_keys = set(self) == set(other)
+        same_values = all((self[k] == other[k]).all(axis=None) for k in self)
+        return same_keys and same_values
--- test/test_xport.py
+++ test/test_xport.py
@@ -2,37 +2,103 @@
 Tests for the core interface.
 """
 
+# Standard Library
+from datetime import datetime
+
 # Community Packages
 import pandas as pd
+import pytest
 
 # Xport Modules
 import xport
 
 
-def test_basic_round_trip_to_from_file(tmp_path):
+def test_set_member_metadata():
     """
-    Verify load/dump are reversible.
+    Verify the Pandas DataFrame accessor allows storing SAS metadata.
     """
-    filepath = tmp_path / 'example.xpt'
-    df = pd.DataFrame({
-        'a': [1, 2],
-        'b': [3, 4],
-    })
-    library = xport.Library(members={'x': xport.Member(observations=df)})
-    with open(filepath, 'wb') as f:
-        xport.dump(library, f)
-    with open(filepath, 'rb') as f:
-        assert library == xport.load(f)
+    metadata = {
+        'name': 'EXAMPLE',
+        'label': 'Example',
+        'type': 'DATA',
+        'created': datetime(2020, 1, 1),
+        'modified': datetime.now(),
+        'os': 'MAC10.15',
+        'version': '9.3',
+    }
+    df = pd.DataFrame()
+    for name, value in metadata.items():
+        setattr(df.sas, name, value)
+    for name, value in metadata.items():
+        assert getattr(df.sas, name) == value
 
 
-def test_basic_round_trip_to_from_string():
+def test_member_metadata_validation():
     """
-    Verify loads/dumps are reversible.
+    Verify the Pandas DataFrame accessor prevents invalid SAS metadata.
     """
-    df = pd.DataFrame({
-        'a': [1, 2],
-        'b': [3, 4],
-    })
-    library = xport.Library(members={'x': xport.Member(observations=df)})
-    s = xport.dumps(library)
-    assert library == xport.loads(s)
+    bad_value = {
+        'name': 'a' * 9,
+        'label': 'a' * 41,
+        'type': 'a' * 9,
+        'created': datetime(1800, 1, 1),
+        'modified': datetime(2100, 1, 1),
+        'os': 'a' * 9,
+        'version': 'a' * 9,
+    }
+    bad_type = {
+        'name': 0,
+        'label': b'',
+        'type': 0,
+        'created': '2000-Jan-01',
+        'modified': 1234,
+        'os': 0,
+        'version': (),
+    }
+    df = pd.DataFrame()
+    for name, value in bad_value.items():
+        with pytest.raises(ValueError):
+            setattr(df.sas, name, value)
+    for name, value in bad_type.items():
+        with pytest.raises((TypeError, AttributeError)):
+            setattr(df.sas, name, value)
+
+
+def test_set_variable_metadata():
+    """
+    Verify the Pandas Series accessor allows storing SAS metadata.
+    """
+    c = pd.Series(['a', 'ab'])
+    c.sas.name = 'EXAMPLE'
+    c.sas.label = 'Example'
+    c.sas.format = '$CHAR10.'
+    c.sas.iformat = '$10.'
+    assert c.sas.name == 'EXAMPLE'
+    assert c.sas.label == 'Example'
+    assert c.sas.type == xport.VariableType.CHARACTER
+    assert c.sas.length == 2
+    assert c.sas.format.name == 'CHAR'
+    assert c.sas.format.length == 10
+    assert c.sas.format.decimals is None
+    assert pd.Series([1]).sas.type == xport.VariableType.NUMERIC
+
+
+def test_variable_metadata_validation():
+    """
+    Verify the Pandas Series accessor prevents invalid SAS metadata.
+    """
+    c = pd.Series(dtype='object')
+    with pytest.warns(UserWarning):
+        c.sas.name = 'a' * 9
+    with pytest.raises(ValueError):
+        c.sas.name = 'a' * 33
+    with pytest.warns(UserWarning):
+        c.sas.label = 'a' * 41
+    with pytest.raises(ValueError):
+        c.sas.label = 'a' * 257
+    with pytest.raises(ValueError):
+        c.sas.format = ''
+    with pytest.raises(ValueError):
+        c.sas.format = '$abcdefghi1.'
+    with pytest.raises(ValueError):
+        c.sas.iformat = '1.2.3'
