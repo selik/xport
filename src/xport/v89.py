3666368ca03f091c27a9b9bf685e2a5d5b0893eb
Mike Selik <mselik@suplari.com>
2020-04-18 22:25:47 -0700

    Note differences between v5, v8, and v9.

--- src/xport/v89.py
+++ src/xport/v89.py
@@ -4,8 +4,11 @@ Read and write the SAS XPORT/XPT file format from SAS Version 8 or 9.
 The SAS V8 Transport File format, also called XPORT, or simply XPT, ...
 """
 
-# Labels may now exceed 40 characters
+# Member names may now be up to 32 characters.
+# Variable names may now be up to 32 characters.
+# Variable labels may now be up to 256 characters.
 # Character fields may now exceed 200 characters.
+# Format fields may now exceed 8 characters (v9).
 
 # Standard Library
 from io import BytesIO
